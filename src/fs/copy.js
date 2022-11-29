416a25d968bfc9538b4a64db92cd42144a2cad66
K.Krutskikh <mintesok@yandex.ru>
2022-11-30 00:47:49 +0300

    create and copy subtask finish

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import { readdir,  mkdir, copyFile  } from 'node:fs/promises'
+
+const path = 'src/fs/'
+
 const copy = async () => {
-    // Write your code here 
+    readdir(path + 'files/').then(files => {
+        if(files.length){
+            mkdir(path + 'files_copy/').catch(() => {
+                throw new Error('FS operation failed')
+            })
+            files.forEach(file => {
+                copyFile(path + `files/${file}`, path + `files_copy/${file}`)
+            })
+        }
+    }).catch(() => {
+        throw new Error('FS operation failed')
+    })
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { appendFile } from 'node:fs/promises'
+
+const path = 'src/fs/files/fresh.txt'
+
 const create = async () => {
-    // Write your code here 
+    appendFile(path, 'I am fresh and young', { flag: 'wx' }).catch(() => {
+        throw Error('FS operation failed')
+    })
 };
 
 await create();
+
+
+
