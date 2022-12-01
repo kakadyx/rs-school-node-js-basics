cdf6bda20c776bb6d7d99a72eedc48762e483b6d
K.Krutskikh <mintesok@yandex.ru>
2022-12-01 23:29:23 +0300

    cli finish: env and args completed

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const myArgs = process.argv.slice(2);
+
+    let argsKeyValueArray = []
+    for(let i = 0; i < myArgs.length; i+=2){
+        argsKeyValueArray.push(`${myArgs[i]} is ${myArgs[i + 1]}`)
+    }
+
+    console.log(argsKeyValueArray.join(', '))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const resultString = Object.entries(process.env).map(([key,value]) => {
+        return `RSS_${key}=${value}`
+    }).join('; ')
+
+    console.log(resultString)
 };
 
 parseEnv();
+
