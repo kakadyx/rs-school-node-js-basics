a40149cd19ea5fe401d1585194c1f33bea70749f
K.Krutskikh <mintesok@yandex.ru>
2022-12-04 14:55:38 +0300

    hash finish

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { createHash } from 'node:crypto'
+import { readFile } from "node:fs/promises";
+const path = 'src/hash/files/fileToCalculateHashFor.txt'
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256')
+    readFile(path).then((data) => {
+        hash.update(data.toString())
+        console.log(hash.digest('hex'))
+    }).catch(() => {
+        throw new Error('FS operation failed')
+    })
 };
 
 await calculateHash();
