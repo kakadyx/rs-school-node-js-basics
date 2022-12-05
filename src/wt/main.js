d280fdd43a6c26c3506820512d9b99abb9a36fc9
K.Krutskikh <mintesok@yandex.ru>
2022-12-05 21:43:36 +0300

    finish worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,22 @@
+import { cpus } from 'os'
+import {Worker} from 'node:worker_threads'
+
 const performCalculations = async () => {
-    // Write your code here
+    const logicalCoresNumber = (Object(cpus()).length)
+    let exittedWorkers = 1
+    const workers = []
+    const result = []
+    for(let i = 0; i < logicalCoresNumber; i++){
+        workers[i] = new Worker('./src/wt/worker.js', {workerData: 10 + i })
+        workers[i]
+            .on('message', msg => result[i] = {status: 'resolved', data: msg})
+            .on('error', err => result[i] = {status: 'error', data: null})
+            .on('exit', code => {
+                if(exittedWorkers++ === logicalCoresNumber){
+                    console.log(result)
+                }
+            })
+    }
 };
 
 await performCalculations();
