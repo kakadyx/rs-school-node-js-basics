cd5979a46e13e122056472e4df642ae39a152904
K.Krutskikh <mintesok@yandex.ru>
2022-12-05 21:06:22 +0300

    finish zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import { brotliCompress } from 'node:zlib'
+import { createReadStream, createWriteStream } from 'fs'
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream('src/zip/files/fileToCompress.txt')
+    readStream.on('data', (data) => {
+        brotliCompress(data, (err,buffer) => {
+            const writeStream = createWriteStream('src/zip/files/archive.gz')
+            writeStream.write(buffer)
+        })
+    })
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { brotliDecompress } from 'node:zlib'
+import {  createReadStream, createWriteStream } from 'fs'
+
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream('src/zip/files/archive.gz')
+    readStream.on('data', (buffer) => {
+        brotliDecompress(buffer, (err,buffer) => {
+            const writeStream = createWriteStream('src/zip/files/fileToCompress.txt')
+            writeStream.write(buffer)
+        })
+    })
 };
 
 await decompress();
